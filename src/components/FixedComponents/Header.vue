14a5e4050b0eabb58452e715549aef6d268e0fda
tricknp <np_patrick@yahoo.com>
2018-09-06 23:34:38 -0300

    init

---
+++ src/components/FixedComponents/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="head">
+
+    <img src="../../../static/images/logo.png" alt="logo" class="head__img">
+
+    <nav class="head__items">
+      <router-link :to="{ name: '' }" class="head__items--item"> 
+        <span>Home</span> 
+        <span class="mark"></span>
+      </router-link>
+      <router-link :to="{ name: '' }" class="head__items--item"> Apart Hotel </router-link>
+      <router-link :to="{ name: '' }" class="head__items--item"> Club </router-link>
+      <router-link :to="{ name: '' }" class="head__items--item"> Gastronomia </router-link>
+      <router-link :to="{ name: '' }" class="head__items--item"> Eventos </router-link>
+      <router-link :to="{ name: '' }" class="head__items--item"> Garopaba </router-link>
+    </nav>
+
+    <button class="head__btn">Book Now</button>
+
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ src/components/GeneralViews/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    
+    <Header />
+
+    <main>
+      <img src="../../../static/images/hotel-generic.jpg" class="home-bg">
+    </main>
+
+  </section>
+</template>
+
+<script>
+import Header from '../FixedComponents/Header'
+
+export default {
+
+  components: { Header }
+
+}
+</script>
