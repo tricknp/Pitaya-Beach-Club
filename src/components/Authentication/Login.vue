972ea7882d81d30a9cec2682b78712e0125f7616
tricknp <np_patrick@yahoo.com>
2018-10-02 15:45:35 -0300

    update login

--- src/components/Authentication/Login.vue
+++ src/components/Authentication/Login.vue
@@ -1,17 +1,13 @@
-<template lang="html">
-  <section>
-    <ul>
-      <li>
-    <input type="text" name="username" v-model="username" placeholder="username">
-    </li>
-      <li>
-    <input type="text" name="password" v-model="password" placeholder="password">
-    </li>
-      <li><button @click="sendLogin()" >login</button></li>
-    </li>
-      <li><span ref="err"></span></li>
-    </ul>
-    </ul>
+<template>
+  <section class="login">
+
+    <form class="login__form">
+      <h2 class="login__form--title">Login</h2>
+      <input class="login__form--input" type="text" v-model="username" placeholder="username">
+      <input class="login__form--input" type="text" v-model="password" placeholder="password">
+      <button class="login__form--btn" @click.prevent="sendLogin()" >Entrar</button>
+      <span ref="err"></span>
+    </form>
 
   </section>
 </template>
@@ -50,49 +46,14 @@ export default {
 };
 </script>
 
-<style lang="css">
-html,
-body,
-#app {
-  height: 100%;
-}
-li {
-  list-style: none;
-  color: darkslategrey;
-}
-ul {
-  border: 1px;
-  border-bottom: 1px;
-  border-style: double;
-  border-color: crimson;
-  width: 30%;
-}
-section {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgb(14, 14, 14);
-  height: 100%;
-  color: rgb(15, 228, 129);
-}
+<style lang="scss">
+  .login
+  {
+    width: 100%;
+    height: 100vh;
+    background: #ddd;
+    &__form{
 
-button {
-  width: 20%;
-  color: rgb(14, 14, 14);
-  background-color: crimson;
-  border-color: crimson;
-  border: 1px;
-  border-bottom: 1px;
-  border-style: double;
-  float: right;
-  cursor: pointer;
-}
-
-input, span {
-  background-color: rgb(14, 14, 14);
-  color: crimson;
-  width: 100%;
-  border: none;
-  outline: none;
-}
+    }
+  }
 </style>
