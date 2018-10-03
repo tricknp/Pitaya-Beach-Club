f4bf19cb4a9e70cafad29b9d94f1e603d30e5fc1
Yuri Bento <yurinbento@hotmail.com>
2018-10-03 13:07:09 -0300

    fixed /login redirect

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,22 +10,3 @@ axios.interceptors.response.use(function (response) {
     return Promise.reject(error);
   }
 });
-
-axios.interceptors.request.use(function (config) {
-  if (config.method != "GET" && !config.url.endsWith('/login')) {
-    //console.log('INTERCEPTOR WORKING...');
-    //console.log(config);
-    let token = localStorage.getItem('token');
-    if (token) {
-      config.headers.authorization = token;
-      return config;
-    } else {
-      window.location = '/login';
-    }
-  } else {
-    return config;
-  }
-}, function (error) {
-  console.log('INTERCEPTOR WORKING...ERROR');
-  return Promise.reject(error);
-});
